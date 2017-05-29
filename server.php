dd485d153242fbafaf2ff74b93c0e461d7d61300
patricio.dilet@gmail.com <F8vsa1ex>
2017-05-28 20:11:31 -0400

    init conf

--- server.php
+++ server.php
@@ -24,7 +24,8 @@ if( $data == null ){
 } else {
 	foreach ($data as $result) {
     if ($result->comuna_nombre == $comuna){
-		 $chatfuel->sendText('' . $result->local_nombre . '<br>test');
+		 $chatfuel->sendText('' . $result->local_nombre . '
+		 test');
 		//  $chatfuel->sendText([
 		// 	 '' . $result->local_nombre . '',
 		// 	 '' . $result->comuna_nombre . '',
